06f4fbea7a1f18a7bd2157a15e0d4743822da060
alepontes <alexandrelopespontes@gmail.com>
2018-04-16 18:29:52 -0300

    Blz, ta melhorando

--- client/script.js
+++ client/script.js
@@ -1,16 +1,59 @@
-//
+const socket = io();
 
-let enviar = document.querySelector('.enviar');
+socket.on('connect', function () {
+    $("#msgs").append(socket.id, ' se conectou', '<br>');
+    socket.emit('msg', socket.id + ' Me conectei');
+});
+
+socket.on('msg', function (msg) {
+    msg_dahora(msg, 0);
+});
 
 
-enviar.onclick = function () {
-    console.log('Teste botão Enviar');
-}//
+$(function () {
+    $("input").keydown(function (key) {
+        if (key.keyCode === 13) {
+            let msg = $(this).val();
+            socket.emit('msg', msg);
+            $(this).val('');
+            msg_dahora(msg, 1);
+        } //if
+    });
+});
 
 
+function msg_dahora(msg, tipo) {
 
-/**
- * Criar não apenas um elemento, mas um grupo html inteiro
- * 
- * 
- */
+    let tipo_msg = '';
+    if (tipo == 0) {
+        tipo_msg = 'rec';
+    } else {
+        tipo_msg = 'env';
+    }
+
+    let $div_msg = $(`<div class='msg ${tipo_msg}'></div>`);
+    let $nome = $(`<p class='nome'>${msg}</p>`);
+    let $texto = $(`<p class="texto">${msg}</p>`);
+
+    $div_msg.append($nome);
+    $div_msg.append($texto);
+
+    $("#msgs").append($div_msg);
+}
+
+function conectou(tipo) {
+
+    let $div_conec = $(`<div class='conectou'></div>`);
+
+    if (tipo == 0) {
+        let titulo = 'Nome';
+    } else {
+        let titulo = 'Você';
+    }
+
+    let $iae_krl = $(`<p class='oi'>${titulo} se conectou</p>`);
+
+    $div_conec.append($iae_krl);
+
+    $("#msgs").append($div_msg);
+}
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/client/chat.html');
 });
 
 io.on('connection', (socket) => {
     socket.on('msg', (msg) => {
-       socket.broadcast.emit('msg', msg);
+        socket.broadcast.emit('msg', msg);
     });
 });
 
 http.listen(3000, function () {
     console.log('Rodando');
 });
+
